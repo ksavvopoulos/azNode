9dd5349b490b5fa0338b83a666bda0ee50113ba9
kostas savvopoulos <ksavvopoulos@gmail.com>
2013-04-25 16:57:07 +0300

    xdir

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
+app.use(express.bodyParser({ uploadDir: __dirname + '/tmp' }));
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
