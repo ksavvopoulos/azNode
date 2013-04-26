16eeca054cf611de5bfc63fd00bdc1194998dd16
kostas savvopoulos <ksavvopoulos@gmail.com>
2013-04-26 15:54:51 +0300

    er

--- app.js
+++ app.js
@@ -22,17 +22,39 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+//app.use(express.logger('dev'));
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
 //app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+//app.use(express.errorHandler());
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+
+//if ('development' == app.get('env')) {
+//  app.use(express.errorHandler());
+//}
+
+function logErrors(err, req, res, next) {
+    console.error(err.stack);
+    next(err);
 }
 
+function clientErrorHandler(err, req, res, next) {
+    if (req.xhr) {
+        res.send(500, { error: err });
+    } else {
+        next(err);
+    }
+}
+
+function errorHandler(err, req, res, next) {
+    res.status(500);
+    res.render('error', { error: err });
+}
 
 //app.get('/', routes.index);
 app.get('/users', user.list);
