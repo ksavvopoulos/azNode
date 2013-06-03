c1434fa5509c219b8c21840598d6b1e00781866b
johnpan <johnnakis@gmail.com>
2013-06-03 16:51:13 +0300

    change json reference

--- public/javascripts/postMessages.js
+++ public/javascripts/postMessages.js
@@ -13,7 +13,7 @@ function InitListener() {
 function SendMessage(theMessage) {
     try {
         var where = window.parent  // to talk to parent frame
-        var myMsg = window.JSON.stringify(theMessage);
+        var myMsg = this.JSON.stringify(theMessage);
         where.postMessage(myMsg, '*');
     } catch (err) {
         alert("SendMessage - Error description: " + err.message);
