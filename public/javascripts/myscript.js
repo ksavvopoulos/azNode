a0a9f60a690daf2119e2b927a89b60bc1b0a345f
johnpan <johnnakis@gmail.com>
2013-06-21 14:15:22 +0300

    rollbk

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -3,6 +3,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
  $(document).ready(
     function(){
         $('input:submit').attr('disabled',true);
@@ -49,6 +50,9 @@ $('#submit').click(function () {
 =======
  
 >>>>>>> parent of eca586d... submit 2
+=======
+ 
+>>>>>>> parent of eca586d... submit 2
 $('#submit').click(function () {
 	if(!$("form input[type=file]").val()) {
         alert('You must select a file!');
@@ -60,8 +64,12 @@ $('#submit').click(function () {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	 
 >>>>>>> parent of 988466d... ready to move to new azure, static vars for service
+=======
+	}
+>>>>>>> parent of eca586d... submit 2
 =======
 	}
 >>>>>>> parent of eca586d... submit 2
