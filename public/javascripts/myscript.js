cd5c35cbd48827a4c4ebbc478d80c526db5c75cb
johnpan <johnnakis@gmail.com>
2013-06-21 14:24:23 +0300

    fixed

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
  $(document).ready(
     function(){
         $('input:submit').attr('disabled',true);
@@ -20,111 +11,21 @@
                     $('input:submit').attr('disabled',true);
                 }
             });
-<<<<<<< HEAD
     });	
-$('#submit').click(function () {	 
-		$('form').hide();
-		$('#img').show();	 
-});	 
+	
 function say(what) { 
     if (window.top != window) { SendMessage({ "theFunction": "say", "theData": what });       
     } else if (window.console) {  console.log(what);  } 
-=======
- 
-$('#submit').click(function () {
-	if(!$("form input[type=file]").val()) {
-        alert('You must select a file!');
-        return false;
-    } else {
-		$('form').hide();
-		$('#img').show();
-	}
-=======
-    });
-	
-=======
- 
->>>>>>> parent of eca586d... submit 2
-=======
- 
->>>>>>> parent of eca586d... submit 2
-=======
- 
->>>>>>> parent of eca586d... submit 2
-=======
- 
->>>>>>> parent of eca586d... submit 2
-=======
- 
->>>>>>> parent of eca586d... submit 2
-=======
- 
->>>>>>> parent of eca586d... submit 2
-=======
- 
->>>>>>> parent of eca586d... submit 2
-=======
- 
->>>>>>> parent of eca586d... submit 2
-=======
- 
->>>>>>> parent of eca586d... submit 2
-$('#submit').click(function () {
-	if(!$("form input[type=file]").val()) {
-        alert('You must select a file!');
-        return false;
-    } else {
-		$('form').hide();
-		$('#img').show();
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	 
->>>>>>> parent of 988466d... ready to move to new azure, static vars for service
-=======
-	}
->>>>>>> parent of eca586d... submit 2
-=======
-	}
->>>>>>> parent of eca586d... submit 2
-=======
-	}
->>>>>>> parent of eca586d... submit 2
-=======
-	}
->>>>>>> parent of eca586d... submit 2
-=======
-	}
->>>>>>> parent of eca586d... submit 2
-=======
-	}
->>>>>>> parent of eca586d... submit 2
-=======
-	}
->>>>>>> parent of eca586d... submit 2
-=======
-	}
->>>>>>> parent of eca586d... submit 2
-=======
-	}
->>>>>>> parent of eca586d... submit 2
-});	
- 
-function say(what) {
-    // Avoids exceptions when console is undefined. 
-    if (window.top != window) {
-        SendMessage({ "theFunction": "say", "theData": what });       
-    } else if (window.console) {
-        console.log(what);
-    } 
-<<<<<<< HEAD
->>>>>>> parent of eca586d... submit 2
-=======
->>>>>>> parent of 988466d... ready to move to new azure, static vars for service
 }
+
+
+$('#submit').click(function () {
+	if(!$("form input[type=file]").val()) {
+        alert('You must select a file!');
+        return false;
+    } else {
+		$('form').hide();
+		$('#img').show();
+	}
+});
+	
