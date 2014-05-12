5571c247974f276adcfd46cc979837d43a144d1b
kostas savvopoulos <ksavvopoulos@gmail.com>
2014-05-12 17:09:52 +0300

    progress4

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -1,41 +1,60 @@
-
- $(document).ready(
-    function(){
-        $('input:submit').attr('disabled',true);
+$(document).ready(
+    function() {
+        $('input:submit').attr('disabled', true);
         $('input:file').change(
-            function(){
-                if ($(this).val()){
-                    $('input:submit').removeAttr('disabled'); 
-                }
-                else {
-                    $('input:submit').attr('disabled',true);
+            function() {
+                if ($(this).val()) {
+                    $('input:submit').removeAttr('disabled');
+                } else {
+                    $('input:submit').attr('disabled', true);
                 }
             });
-    });	
-	
-function say(what) { 
-    if (window.top != window) { SendMessage({ "theFunction": "say", "theData": what });       
-    } else if (window.console) {  console.log(what);  } 
+    });
+
+function say(what) {
+    if (window.top != window) {
+        SendMessage({
+            "theFunction": "say",
+            "theData": what
+        });
+    } else if (window.console) {
+        console.log(what);
+    }
 }
 
 
-$('#submit').click(function () {
-    var $form = $('form');
+// $('#submit').click(function () {
+//     var $form = $('form');
 
-	if(!$("form input[type=file]").val()) {
+// 	if(!$("form input[type=file]").val()) {
+//         alert('You must select a file!');
+//         return false;
+//     } else {
+
+// 		$form.hide();
+// 		$('#img').show();
+// 	}
+// });
+
+$('form').on('submit', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+
+    if (!$("form input[type=file]").val()) {
         alert('You must select a file!');
         return false;
     } else {
-        getProgress();
-		$form.hide();
-		$('#img').show();
-	}
-});
+        $this.ajaxSubmit({
+            url: '/upload',
+            uploadProgress: function(e, p, t, p) {
+                say(e);
+                say(p);
+                say(t);
+                say(p);
+            },
+        });
 
-function getProgress(){
-    $.get('/progress').done(function(progress){
-        say(progress);
-        setTimeout(getProgress,50);
-    });
-}
-	
+        $this.hide();
+        $('#img').show();
+    }
+});
